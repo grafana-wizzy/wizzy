f1678990eb3fe7f165ea45c67c26e60dfe0ab09d
utkarshcmu <utkarsh.cmu@gmail.com>
2016-11-21 17:23:27 -0800

    Refactored index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ var fs    = require('fs');
 var nconf = require('nconf');
 var confFile = 'conf/wizzy.json';
 
+var help = '\nUsage: wizzy [commands]\n\ncCommands:';
+
 nconf.argv()
 	.env()
  	.file({ file: confFile });
@@ -18,31 +20,33 @@ var program = require('commander');
 program
   .version('1.0.0');
 
-program
-	.command('init')
-  .action(init);
+addCommand(program, 'init', init, 'wizzy init GRAFANA_URL',
+	'initializes and connects wizzy to Grafana', 'wizzy init http://localhost:3000');
 
-program
-	.command('reset')
-	.action(reset);
+addCommand(program, 'reset', reset, 'wizzy reset GRAFANA_URL',
+	'resets Grafana URL in config', 'wizzy reset http://localhost:6000');
 
-program
-	.command('help')
-  .action(help);
+addCommand(program, 'help', showHelp, 'wizzy help',
+	'shows all available commands');
 
-program
-	.command('conf')
-  .action(showConfig);
+addCommand(program, 'conf', showConfig, 'wizzy conf',
+	'shows wizzy configuration');
+
+addCommand(program, 'create', createEntity, 'wizzy create ENTITY ENTITY_NAME',
+	'creates a new entity like a new org or a new dashboard', 'wizzy create dashboard my-dash');
+
+addCommand(program, 'use', useEntity, 'wizzy use ENTITY ENTITY_NAME',
+	'points context to an existing entity', 'wizzy use org my-org');
 
 program
 	.command('*')
-  .action(help);
+  .action(showHelp);
 
 program
 	.parse(process.argv);
 
 if (process.argv.length < 3) {
-	hello();
+	showHelp();
 }
 
 function init() {
@@ -65,39 +69,24 @@ function init() {
 	}
 }
 
-function help() {
-	console.log('\nUsage: wizzy [commands]');
-	console.log('\n Commands:');
-	console.log('\n  wizzy init GRAFANA_URL')
-	console.log('\t- initializes and connects wizzy to Grafana');
-	console.log('\t- Example: wizzy init http://localhost:3000');
-	console.log('\n  wizzy create org ORG_NAME');
-	console.log('\t- creates a new org in Grafana');
-	console.log('\t- Example: wizzy create org my_org');
-	console.log('\n  wizzy use org ORG_NAME');
-	console.log('\t- switches wizzy\'s context to an org');
-	console.log('\n  wizzy create dashboard DASHBOARD_NAME');
-	console.log('\t- creates a new dashboard');
-	console.log('\n  wizzy use dashboard DASHBOARD_NAME');
-	console.log('\t- switches wizzy\'s context to a dashboard');
-	console.log('\n  wizzy reset GRAFANA_URL')
-	console.log('\t- resets Grafana URL in config');
-	console.log('\t- Example: wizzy reset http://localhost:6000');
-	console.log('\n  wizzy help')
-	console.log('\t- shows all available commands');
-	console.log('\n');
+// Prints help for the user
+function showHelp() {
+	console.log(help);
 }
 
+// Prints wizzy config
 function showConfig() {
 	console.log(prettyjson.render(nconf.get('config')));
 }
 
+// Resets Grafana URL
 function reset() {
 	nconf.set('config:grafana:url', process.argv[3]);
 	saveConfig();
 	console.log("Configuration updated successfully.")
 }
 
+// Save wizzy config
 function saveConfig() {
 	nconf.save(function (err) {
   	fs.readFile(confFile, function (err, data) {
@@ -106,4 +95,32 @@ function saveConfig() {
     	}
   	});
 	});
+}
+
+// Adds a command to wizzy cli
+function addCommand(program, command, func, syntax, description, example) {
+	
+	// Adding command to command line tool
+	program
+		.command(command)
+  	.action(func);
+
+  // Adding command to help
+  help += '\n  ' + syntax;
+  if (description != null) {
+		help += '\n\t- ' + description;
+	}
+	if (example != null) {
+		help += '\n\t- Example: ' + example;
+	}
+}
+
+// Creates an entity
+function createEntity() {
+	console.log("Creating entity");
+}
+
+// Updates context with an existing entity
+function useEntity() {
+	console.log("Using entity");
 }
